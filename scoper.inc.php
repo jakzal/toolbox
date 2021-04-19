7fb1677aeadb5eb46fe779bc435ef81f2ce7cd53
Jakub Zalas <jakub@zalas.pl>
2021-04-19 15:01:00 +0100

    Do not scope PHP 8 keywords

--- scoper.inc.php
+++ scoper.inc.php
@@ -1,4 +1,24 @@
 <?php declare(strict_types=1);
 
 return [
+    'patchers' => [
+        function (string $filePath, string $prefix, string $content): string {
+            if (!Strings::contains($filePath, 'vendor/')) {
+                return $content;
+            }
+
+            $fqcnReservedPattern = sprintf('#(\\\\)?%s\\\\(parent|self|static)#m', $prefix);
+            $matches = Strings::matchAll($content, $fqcnReservedPattern);
+
+            if (!$matches) {
+                return $content;
+            }
+
+            foreach ($matches as $match) {
+                $content = str_replace($match[0], $match[2], $content);
+            }
+
+            return $content;
+        },
+    ],
 ];

713f653e1e68015345c6cc797f8c9d7fa2f9d6ef
Jakub Zalas <jakub@zalas.pl>
2021-04-19 19:31:57 +0100

    Whitelist Symfony polyfills (#386)

--- scoper.inc.php
+++ scoper.inc.php
@@ -1,4 +1,11 @@
 <?php declare(strict_types=1);
 
 return [
+    // Whitelist globals so that Symfony polyfills are not scoped
+    'whitelist-global-constants' => true,
+    'whitelist-global-classes' => true,
+    'whitelist-global-functions' => true,
+    'files-whitelist' => [
+        'vendor/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+    ],
 ];

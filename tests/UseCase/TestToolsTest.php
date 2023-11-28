f866e4de58f03586ce3c941f88012e78b33d3700
Jakub Zalas <jakub@zalas.pl>
2023-11-28 11:13:51 +0000

    Replace prophecy in TestToolsTest

--- tests/UseCase/TestToolsTest.php
+++ tests/UseCase/TestToolsTest.php
@@ -3,9 +3,9 @@
 namespace Zalas\Toolbox\Tests\UseCase;
 
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Zalas\Toolbox\Tool\Collection;
 use Zalas\Toolbox\Tool\Command;
+use Zalas\Toolbox\Tool\Command\ShCommand;
 use Zalas\Toolbox\Tool\Filter;
 use Zalas\Toolbox\Tool\Tool;
 use Zalas\Toolbox\Tool\Tools;
@@ -13,8 +13,6 @@ use Zalas\Toolbox\UseCase\TestTools;
 
 class TestToolsTest extends TestCase
 {
-    use ProphecyTrait;
-
     public function test_it_returns_test_aggregated_test_command()
     {
         $testCommands = [
@@ -31,30 +29,28 @@ class TestToolsTest extends TestCase
 
     private function tool(Command $testCommand): Tool
     {
-        $tool = $this->prophesize(Tool::class);
-        $tool->testCommand()->willReturn($testCommand);
-        $tool->tags()->willReturn([]);
-
-        return $tool->reveal();
+        return new Tool(
+            "any name",
+            "any summary",
+            "https://example.com",
+            [],
+            new Command\ShCommand("any command"),
+            $testCommand
+        );
     }
 
     private function command(string $command): Command
     {
-        $c = $this->prophesize(Command::class);
-        $c->__toString()->willReturn($command);
-
-        return $c->reveal();
+        return new ShCommand($command);
     }
 
     private function tools(array $testCommands, Filter $filter): Tools
     {
-        $tools = $this->prophesize(Tools::class);
-        $tools->all($filter)->willReturn(Collection::create([
-            $this->tool($testCommands[0]),
-            $this->tool($testCommands[1]),
-        ]));
-
-        return $tools->reveal();
+        $tools = $this->createStub(Tools::class);
+        $tools->method('all')->with($filter)->willReturn(Collection::create(
+            array_map(fn ($command) => $this->tool($command), $testCommands)
+        ));
+        return $tools;
     }
 
     private function filter(): Filter
